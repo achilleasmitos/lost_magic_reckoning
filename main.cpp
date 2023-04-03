2e3ba16aada27fe82fc501fd1a07eff03e38c1b3
Achilleas Mitos <axmito@gmail.com>
2023-04-03 23:59:42 +0300

    Utils and print - Added namespace utils - Added comments to utils file - Added a way to adjust the text speed when starting the game

--- main.cpp
+++ main.cpp
@@ -11,27 +11,33 @@ int main()
     // Creature archer;
     // archer.print_stats();
 
+    // std::cout << "=======================" << std::endl;
+
+    std::cout << "Give me the time you want between each rendered character (in ms): ";
+    std::cin >> utils::sleep_for_ms;
+
     MainCharacter main_character("Achilleas Mitos");
 
     std::string new_name;
     std::cout << "Give me a new name for your character: ";
-    get_sanitized_string(new_name);
+    utils::get_sanitized_string(new_name);
     main_character.set_name(new_name);
     main_character.print_stats();
 
     std::cout << "Give me the name for another character: ";
-    get_sanitized_string(new_name);
+    utils::get_sanitized_string(new_name);
 
     MainCharacter main_2(new_name, 15, 14, 7, 16, 13, 14);
     main_2.print_stats();
 
     std::cout << "Give me the name for another character: ";
-    get_sanitized_string(new_name);
+    utils::get_sanitized_string(new_name);
+
 
     MainCharacter main_3(new_name, 12, 14, 35, 16, 14, 10, 8, 7, 12, "humanoid");
     main_3.print_stats();
 
-    print({"This will ", "appear ", std::to_string(3), " times slower", "..."}, 300);
+    utils::print({"This will ", "appear ", std::to_string(3), " times slower", "..."}, 3 * utils::sleep_for_ms);
 
     system("pause");
     return 0;
