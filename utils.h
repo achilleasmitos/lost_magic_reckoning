feba5ddc773d478a508d06e019cd8f6acd79e95b
Achilleas Mitos <axmito@gmail.com>
2023-04-03 09:51:24 +0300

    Created print function

--- utils.h
+++ utils.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <thread>
+#include <chrono>
 
 void get_sanitized_string(std::string &string)
 {
@@ -10,4 +12,23 @@ void get_sanitized_string(std::string &string)
         std::cin.ignore();
     }
     std::getline(std::cin, string);
+}
+
+/**
+ * @brief Function to print a list of strings to the console, pausing in-between characters.
+ *
+ * @param list A list of std::strings to std::cout
+ * @param sleep_for_ms (optional) The amount of milliseconds to sleep between characters.
+ */
+void print(std::initializer_list<std::string> list, int sleep_for_ms = 100)
+{
+    for (auto string : list)
+    {
+        for (auto character : string)
+        {
+            std::cout << character;
+            std::this_thread::sleep_for(std::chrono::milliseconds(sleep_for_ms));
+        }
+    }
+    std::cout << std::endl;
 }
